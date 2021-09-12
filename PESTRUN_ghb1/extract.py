6a506cda5cc475b5af5c4f5893bb904f4bf06a4a
jdub <JWhite@localadmins-MacBook-Pro.local>
2021-09-12 17:57:03 -0600

    added the completed time-to-thres function

--- PESTRUN_ghb1/extract.py
+++ PESTRUN_ghb1/extract.py
@@ -16,12 +16,14 @@ def extract_results():
 	"""
 
 	import os
+	import numpy as np
 	import pandas as pd
+
 	import flopy
 
 
 	# the results files to process - the extension is important!
-	results_files = ["sa00_U.con"]#, "sa00_U.hds", "sa00_flow.hds"]
+	results_files = ["sa00_U.con","sa00_U.hds", "sa00_flow.hds"]
 
 	# the csv files of point locations of interest.
 	pts_files = ["sa00_TransportModel_conctargets.csv", "sa00_TransportModel_headtargets.csv",
@@ -54,7 +56,7 @@ def extract_results():
 	df.index = df.apply(lambda x: "name:{0}_k:{1}_j:{2}_k:{3}".format(x.site_name.lower(),x.k,x.i,x.j),axis=1)
 	df.index.name = "site_name"
 
-	thres = 0.85
+	thres = 0.85 # from Bill; in model units
 	# now process each results file
 	for results_file in results_files:
 		# here is where the extension matters:
@@ -76,11 +78,38 @@ def extract_results():
 		# concat all the output time dfs into one df and save
 		df_kper = pd.concat(dfs,axis=1)
 		df_kper.to_csv(results_file.lower()+"_processed.csv")
-
 		if results_file.lower().endswith("con"):
-			print(hds.get_times())
+			# transpose to get times on the index
+			df_kper = df_kper.T
 
-	
+			# set the index to totim values
+			df_kper.index = hds.get_times()
+			df_kper.index.name = "totim"
+			
+			# some plotting just for testing
+			#import matplotlib.pyplot as plt
+			#fig,ax = plt.subplots(1,1)
+			#df_kper.loc[:,"name:eastwell_k:11_j:103_k:27"].plot(ax=ax)
+			
+
+			# reindex to daily across the full sim range - has lots of nans
+			df_reindex = df_kper.reindex(np.arange(0,max(hds.get_times())))
+			# now fill those nans with interpolated values - cubic spline looks pretty good...
+			df_reindex = df_reindex.interpolate(method="cubicspline")
+			# backfill from the first output time to the beginning of the sim
+			df_reindex.fillna(method="bfill",inplace=True)
+
+			#df_reindex.loc[:,"name:eastwell_k:11_j:103_k:27"].plot(ax=ax)
+			#plt.show()
+			with open("time_to_thres.processed.csv",'w') as f:
+				f.write("site,days_to_thres\n")
+				for site in df_reindex.columns:
+					df_site = df_reindex.loc[:,site].copy()
+					df_site = df_site.loc[df_site.values>=thres]
+					if df_site.shape[0] == 0:
+						f.write("{0},{1:15.6E}\n".format(site,0.0))
+					else:
+						f.write("{0},{1:15.6E}\n".format(site,df_site.index[0]))
 
 
 
